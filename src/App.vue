b014de9827e11a01d2dfaeb00c3ea9feeef53ee5
bad4iz <develop@bad4iz.ru>
2021-03-29 21:47:56 +0400

    main

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,33 @@
     <v-app-bar
       app
       :clipped-left="getPrimaryDrawer.clipped"
-      dark
       absolute
-      color="red darken-4"
+      flat
+      class="header"
+      elevate-on-scroll
     >
       <v-app-bar-nav-icon
         v-if="getPrimaryDrawer.type !== 'permanent'"
         @click.stop="setPrimaryDrawerAction({ model: !getPrimaryDrawer.model })"
       />
-      <v-toolbar-title>{{ $appConfig.titleApp }}</v-toolbar-title>
+      <v-toolbar-title class="title" @click="()=>$router.push({ path: '/' })">{{ $appConfig.titleApp }}</v-toolbar-title>
+
+      <v-spacer />
+
+      <v-list dense class="menu">
+        <v-list-item to="/">
+          <v-list-item-content>
+            <v-list-item-title>Посты</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/settings-app">
+          <v-list-item-content>
+            <v-list-item-title>Философия</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-spacer />
     </v-app-bar>
 
     <v-main>
@@ -57,3 +75,15 @@
     },
   };
 </script>
+
+<style scoped>
+  header.v-toolbar.v-app-bar.header {
+    background-color: inherit;
+  }
+  .menu {
+    display: flex;
+  }
+  .title {
+    cursor: pointer;
+  }
+</style>
