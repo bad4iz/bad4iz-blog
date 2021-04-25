9d7bb16094783860512c2beb2e3a61e5c25ef53d
bad4iz <developer@bad4iz.ru>
2021-04-25 15:07:41 +0400

    немного кросивостей

---
+++ resources/js/components/system/Icon.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-icon>{{ path }}</v-icon>
+</template>
+
+<script>
+import SvgIcon from '@jamescoyle/vue-icon';
+import * as icons from '@mdi/js';
+
+export default {
+    name: 'Icon',
+    components: {
+        SvgIcon
+    },
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+    },
+    data() {
+        return {
+            path: icons[this.name],
+        };
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/components/widgets/MementoMori.Widget.vue
+++ resources/js/components/widgets/MementoMori.Widget.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-card class="mt-16" to="/memento-mori">
+    <v-card class="mt-16" >
         <div class="relative bg-white overflow-hidden">
             <div class="max-w-7xl mx-auto">
                 <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
@@ -24,7 +24,9 @@
                     <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                         <div class="sm:text-center lg:text-left">
                             <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                                <span class="block xl:inline">Memento Mori</span> <br>
+                                <span class="block xl:inline">Memento Mori</span>
+                            </h1>
+                            <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                                 <span class="block text-indigo-600 xl:inline">помни о смерти</span>
                             </h1>
                             <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
@@ -32,6 +34,16 @@
                             </p>
                             <p>СИНЕКА</p>
                         </div>
+                        <v-card-actions>
+                            <v-btn
+                                class="ml-auto"
+                                outlined
+                                color="primary"
+                                to="/memento-mori"
+                            >
+                                Подробнее
+                            </v-btn>
+                        </v-card-actions>
                     </main>
                 </div>
             </div>
@@ -41,7 +53,6 @@
                 </div>
             </div>
         </div>
-
     </v-card>
 </template>
 
