30498e3820dd3e7e817d8eefa57378144978b53e
bad4iz <developer@bad4iz.ru>
2021-07-12 08:24:06 +0400

    book view

--- database/migrations/2021_07_09_163703_create_books_table.php
+++ database/migrations/2021_07_09_163703_create_books_table.php
@@ -20,6 +20,7 @@ class CreateBooksTable extends Migration
             $table->longText('description')->nullable();
             $table->string('isbn')->nullable();
             $table->string('img')->nullable();
+            $table->string('reading')->nullable();
             $table->unsignedInteger('year_of_publishing')->nullable();
             $table->unsignedInteger('rating')->default(0)->nullable();
             $table->unsignedInteger('type_book_id')->default(0);
---
+++ resources/js/views/BooksRead.View.vue
@@ -0,0 +1,85 @@
+<template>
+    <v-card class="my-9">
+        <v-img
+            height="200px"
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsun9-8.userapi.com%2Fc841532%2Fv841532431%2F9b9c%2FPuMLU_hRcfY.jpg&f=1&nofb=1"
+        >
+
+            <v-card-title class="white--text mt-8">
+                <v-avatar size="56">
+                    <img
+                        alt="user"
+                        src="https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg"
+                    >
+                </v-avatar>
+                <p class="ml-3">
+                    Последние прочитанные книги
+                </p>
+            </v-card-title>
+        </v-img>
+
+        <v-card-text>
+            <v-timeline>
+                <ApolloQuery :query="LastBookReadingProgress">
+                    <template
+                        slot-scope="{ result: { data: {lastBookReadingProgress : {data} = {}} = {}, loading }, isLoading }">
+                        <div v-if="isLoading">Loading...</div>
+
+                        <v-timeline-item
+                            v-else
+                            v-for="item in data"
+                            :key="item.id"
+                            color="green"
+                        >
+                            <template v-slot:opposite>
+                                <span
+                                    class="headline font-weight-bold ${year.color}--text"
+                                    v-text="item.date"
+                                ></span>
+                            </template>
+                            <v-card>
+                                <div class="d-flex flex-no-wrap ">
+
+                                    <v-img
+                                        class="ma-2"
+                                        max-height="150"
+                                        max-width="150"
+                                        contain
+                                        :src="item.book.img"/>
+                                    <div>
+
+                                        <v-card-title
+                                            class="text-h5"
+                                            v-text="item.book.name"
+                                        />
+
+                                        <v-card-subtitle v-text="item.book.author"/>
+                                        <v-card-subtitle v-text="item.verdict"/>
+                                    </div>
+                                </div>
+                            </v-card>
+                        </v-timeline-item>
+                    </template>
+                </ApolloQuery>
+
+            </v-timeline>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+import LastBookReadingProgress from '@/graphql/queries/LastBookReadingProgress.gql';
+
+export default {
+    name: 'BooksReadView',
+    data: () => ({
+        LastBookReadingProgress,
+        messages: [
+        ],
+    }),
+};
+</script>
+
+<style scoped>
+
+</style>
