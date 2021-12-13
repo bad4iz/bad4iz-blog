ddeefe14d4f0ccacda9cf7eea16dc7c9b3e886c4
bad4iz <developer@bad4iz.ru>
2021-12-13 22:34:57 +0400

    график

--- resources/js/components/widgets/ContinuousConsumptionConcept.Widget.vue
+++ resources/js/components/widgets/ContinuousConsumptionConcept.Widget.vue
@@ -1,38 +1,38 @@
 <template>
-    <v-card class="mt-16" >
-        <h1> Концепция постоянного потребления на протяжении жизни человека </h1>
+    <v-card class="mt-16 norelative">
+            <h1> Концепция постоянного потребления на протяжении жизни человека </h1>
 
-        <h3> Сколько можно тратить в год {{pr(summ)}} </h3>
-        <h3> Сколько надо откладывать в год {{ pr(annualProfit - summ) }} </h3>
-<!--        <D3LineChart :config="chartConfig" :datum="chartData"></D3LineChart>-->
-        <v-card-text>
-            <div>
-                <v-text-field
-                    label="Годовая прибыль"
-                    type="number"
-                    v-model="annualProfit"
-                ></v-text-field>
-                <v-slider
-                    v-model="proc"
-                    :label="`процентная ставка: ${proc}`"
-                    min="1"
-                    max="60"
-                ></v-slider>
-                <v-slider
-                    v-model="yearsBeforeRetirement"
-                    :label="`Количество лет до пенсии: ${yearsBeforeRetirement}`"
-                    min="1"
-                    max="60"
-                ></v-slider>
-                <v-slider
-                    v-model="yearsAfterRetirement"
-                    :label="`Количество лет после пенсии: ${yearsAfterRetirement}`"
-                    min="1"
-                    max="60"
-                ></v-slider>
+            <h3> Ежегодная норма потребления {{ pr(summ) }} </h3>
+            <h3> Сколько надо откладывать в год {{ pr(annualProfit - summ) }} </h3>
+            <D3LineChart :config="chartConfig" :datum="chartData"></D3LineChart>
+            <v-card-text>
+                <div>
+                    <v-text-field
+                        label="Годовая прибыль"
+                        type="number"
+                        v-model="annualProfit"
+                    ></v-text-field>
+                    <v-slider
+                        v-model="proc"
+                        :label="`процентная ставка: ${proc}`"
+                        min="1"
+                        max="60"
+                    ></v-slider>
+                    <v-slider
+                        v-model="yearsBeforeRetirement"
+                        :label="`Количество лет до пенсии: ${yearsBeforeRetirement}`"
+                        min="1"
+                        max="60"
+                    ></v-slider>
+                    <v-slider
+                        v-model="yearsAfterRetirement"
+                        :label="`Количество лет после пенсии: ${yearsAfterRetirement}`"
+                        min="1"
+                        max="60"
+                    ></v-slider>
 
-            </div>
-        </v-card-text>
+                </div>
+            </v-card-text>
     </v-card>
 </template>
 
@@ -48,7 +48,7 @@ export default {
         annualProfit: 1000000,
         chartData: [],
         chartConfig: {
-            values: ['FV'],
+            values: [ 'save', 'T', 'sum'],
             date: {
                 key: 'year',
                 inputFormat: '%Y',
@@ -58,38 +58,79 @@ export default {
                 ease: 'easeBounceOut',
                 duration: 100,
             },
-            axis: {
-                yFormat: '.1s',
-                yTitle: 'Накоплено денег',
-            },
             margin: {
                 top: 20,
                 right: 20,
                 bottom: 20,
-                left: 70,
+                left: 90,
+            },
+            tooltip: { labels: [ "Копим", "Осталось", 'Наше постоянное потребление'] },
+            axis: {
+                yFormat: '.1s',
+                yTitle: 'Накоплено денег',
+            },
+            color: {
+                scheme: ['#55D6BE', '#e50f7c', '#0838e5'],
             },
         },
     }),
     methods: {
         pr(v) {
-            const value =  v.toFixed(0);
-            return [...value].reverse().map((a, idx)=> (idx+1) % 3 === 0 ? ' ' + a : a).reverse().join('');
-        }
+            const value = v.toFixed(0);
+            return [...value].reverse().map((a, idx) => (idx + 1) % 3 === 0 ? ' ' + a : a).reverse().join('');
+        },
+        chartDataClear() {
+            while (this.chartData.length) {
+                this.chartData.pop();
+            }
 
+        },
+        getFutureAnnuity(n) {
+            const r = Number(this.r);
+            return ((1 + r) ** Number(n) - 1) / r;
+        },
+        getAnnuity(n) {
+            const r = Number(this.r);
+            return Math.abs(1 - (1 / ((1 + r) ** n))) / r;
+        }
     },
     computed: {
         r() {
-          return +this.proc/100
+            return +this.proc / 100;
         },
         futureAnnuity() {
-            return ( (1+ Number(this.r))**Number(this.yearsBeforeRetirement) - 1  ) / this.r;
+            return this.getFutureAnnuity(this.yearsBeforeRetirement);
         },
         annuity() {
-            return Math.abs(1 - (1 / ((1 + this.r) ** this.yearsAfterRetirement))) / this.r;
+            return this.getAnnuity(this.yearsAfterRetirement);// Math.abs(1 - (1 / ((1 + this.r) ** this.yearsAfterRetirement))) / this.r;
         },
         summ() {
-          return (this.annualProfit * this.futureAnnuity) / (this.annuity + +this.futureAnnuity )
-      }
+            this.chartDataClear();
+            const sum = (this.annualProfit * this.futureAnnuity) / (this.annuity + +this.futureAnnuity);
+
+            // до пенсии
+            for (let i = 0; i <= this.yearsBeforeRetirement; i++) {
+                this.chartData.push({
+                    save: Math.floor((this.annualProfit - sum) * this.getFutureAnnuity(i)) || 0,
+                    // total: this.annualProfit * this.getFutureAnnuity(i) || 0,
+                    T: 0,
+                    sum: Math.floor(sum),
+                    year: i,
+                });
+            }
+            // после пенсии
+            const a = this.yearsAfterRetirement + this.yearsBeforeRetirement;
+            for (let i = this.yearsBeforeRetirement; i <= a; i++) {
+                this.chartData.push({
+                    T: Math.floor(sum * this.getAnnuity(a - i)) || 0,
+                    // total: this.annualProfit * this.getFutureAnnuity(i) || 0,
+                    sum: Math.floor(sum),
+                    save: 0,
+                    year: i,
+                });
+            }
+            return sum;
+        }
     },
     components: {
         D3LineChart,
@@ -97,6 +138,8 @@ export default {
 };
 </script>
 
-<style scoped lang="sass">
-
+<style scoped >
+.norelative {
+    position: unset;
+}
 </style>
